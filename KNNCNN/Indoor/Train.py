74f4d8dfce38bdbe8d3bed77f364b047ed34b9c5
kvuckovic <71682568+kvuckovic@users.noreply.github.com>
2021-01-20 21:46:12 -0500

    Update Train.py

--- KNNCNN/Indoor/Train.py
+++ KNNCNN/Indoor/Train.py
@@ -16,35 +16,33 @@ import sys
 import numpy as np
 import statistics
 
-
-
+#Preparing Data
+#Get DCNN training data that consists of (ADP, location) pairs
 data_path1 ='Data/TrainDCNNI1.npz'
 data1 = load(data_path1)
 data = data1
+# Define size of (X,Y)grid for the area of interest
+# xnum - equally space segnments in x-diretion, ynum - equally spaced segnments in y-direction
 xnum = 18
 ynum = 55
-num_classes = xnum * ynum
-L = data1['Loc']
-x = L[:,0]
-y = L[:,1]
-xmax = max(x)
-xmin = min(x)
-ymax = max(y)
-ymin = min(y)
-print(xmax-xmin)
-print(ymax-ymin)
-print(L.shape)
+num_classes = xnum * ynum #total number of CNN classes based on grid dimensions
+
+# Function assign classes to each ADP based on the location on the grid
+# The output is a training dataset and testing dataset.  Each dataset consists of (ADP, class) pairs
 def get_data(data,xnum,ynum):
     num_classes = xnum*ynum
-    #get train data
+    # Get ADP and Location pairs from training dataset
     M = np.reshape(data['ADP'],(66550,32,32,1))
     loc = data['Loc']
     x = loc[:,0]
     y = loc[:,1]
+    # Create placeholders for class (grid) and new (X,Y) coordiantes which are at the center of each .
+    # Example: If the user is in the mth segment in x-direaction and nth segment in y-direction, the new coordinates are (m,n) and the sample belongs to class c = m*n.
     cnew = np.zeros((len(loc),num_classes))
     c = np.zeros(len(loc))
     xnew = np.zeros(len(x))
     ynew = np.zeros(len(y))
+     # Calcualte step size based on the grid
     xmax = max(x)
     xmin = min(x)
     xstep = (xmax-xmin)/xnum
@@ -52,8 +50,7 @@ def get_data(data,xnum,ynum):
     ymin = min(y)
     ystep = (ymax-ymin)/ynum
 
-    #This is for x
-    #print('x: ',x[1:10])
+    # Convert x coordinate to grid segment in x
     for i in range(len(x)):
         for j in range(1,xnum+1):
             low = xmin + (j-1)*xstep
@@ -68,9 +65,7 @@ def get_data(data,xnum,ynum):
             print(xmin)
             sys.exit()
 
-    #print('xnew:',xnew[1:10])
-    #This is for y
-    #print('y: ',y[1:10])
+    # Convert y coordinate to grid segment in y
     for i in range(len(y)):
         for j in range(1,ynum+1):
             low = ymin + (j-1)*ystep
@@ -84,22 +79,32 @@ def get_data(data,xnum,ynum):
             print(ymax)
             print(ymin)
             sys.exit()
-    #print('ynew: ',ynew[1:10])
-    #This creates a class
+    
+     # Assigns classes to dataset based on grid starting with grid(1,1) assigned to c c=1 
+    # and grid (m,n) assigned to class c = m*n.
     for i in range(len(loc)):
         c[i] = (xnew[i]-1)+xnum*(ynew[i]-1)
     c = np.reshape(c,(66550,1))
     
+     # Reshapes class matrix to be in appriorate format for further processing
     for i in range(len(c)):
       cnew[i,int(c[i])] = 1  
     print('cnew shape: ',cnew.shape)
-   # cnew = cnew*0.01 #class is in range 0 to 1
-
+   
+    #Splits data consisting of (ADP,class) pairs randomly into 90% train and 10% test
     train_ADP, test_ADP, train_Loc, test_Loc = train_test_split(M, cnew, test_size=0.1, random_state=42)
-
+     
+    # Function returns 
+    # loc - location (x,y) aentire dataset
+    # c - class corresponding to loc for etire dataset
+    # train_ADP and train_Loc - training dataset pair ADP and class
+    # test_ADP and test_Loc - testing dataset pair ADP and class
     return loc,c,train_ADP, test_ADP, train_Loc, test_Loc
+
+# Get traing and testing datasets
 loc, c, xtrain,xtest,ytrain,ytest = get_data(data,xnum,ynum)
 
+#Training CNN
 from keras.layers import Dense, Activation, Flatten, Conv2D, BatchNormalization, MaxPool2D
 model = Sequential()
 # L = 1
