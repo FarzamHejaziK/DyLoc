6470da993e90f35642b3e21a23489634ce68f720
kvuckovic <71682568+kvuckovic@users.noreply.github.com>
2021-01-20 23:08:44 -0500

    Update MakeADPgrid.py

--- KNNCNN/Indoor/MakeADPgrid.py
+++ KNNCNN/Indoor/MakeADPgrid.py
@@ -1,14 +1,19 @@
 import numpy as np
 
+# Define x and y grid size. Must match xnum and ynum in the Train.py
 xnum = 18
 ynum = 55
 
-
+#Function gets validation data and returns (ADP, class) pairs
+# Inputs: raw validation data (ADP, location), and size of (x,y0 grid
+# t - handles ame of location dataset. Can be removed if name of locaiton column is known
+#Outputs: (ADP, class) pairs and new location based on classes (similar to get_data in tarin.py)
 def get_valid_data(data,xnum,ynum,t):
+  # calculate total number of classes
   num_classes = xnum*ynum
   adp = data['ADP']
   print('adp shape ',adp.shape)
-  #get train data
+  # Get DP and (x,y0 location from dataset
   M = np.reshape(adp,(-1,32,32))
   if t ==0 :
     loc = data['Location']
@@ -16,10 +21,14 @@ def get_valid_data(data,xnum,ynum,t):
     loc = data['Loc']
   x = loc[:,0]
   y = loc[:,1]
+  # Create placeholders for class (grid) and new (X,Y) coordiantes which are at the center of each grid.
+  # Example: If the user is in the mth segment in x-direaction and nth segment in y-direction, the new coordinates are (m,n) and the sample belongs to class c = m*n.
   cnew = np.zeros((len(loc),num_classes))
   c = np.zeros(len(loc))
   xnew = np.zeros(len(x))
   ynew = np.zeros(len(y))
+  
+   # Calcualte step size based on the grid
   xmax = max(x)
   xmin = min(x)
   xstep = (xmax-xmin)/xnum
@@ -27,8 +36,7 @@ def get_valid_data(data,xnum,ynum,t):
   ymin = min(y)
   ystep = (ymax-ymin)/ynum
 
-  #This is for x
-  #print('x: ',x[1:10])
+  # Convert x coordinate to grid segment in x
   for i in range(len(x)):
       for j in range(1,xnum+1):
           low = xmin + (j-1)*xstep
@@ -43,7 +51,7 @@ def get_valid_data(data,xnum,ynum,t):
           print(xmin)
           sys.exit()
 
-
+ # Convert y coordinate to grid segment in y
   for i in range(len(y)):
       for j in range(1,ynum+1):
           low = ymin + (j-1)*ystep
@@ -58,15 +66,20 @@ def get_valid_data(data,xnum,ynum,t):
           print(ymin)
           sys.exit()
 
-  #This creates a class
+   # Assigns classes to dataset based on grid starting with grid(1,1) assigned to c c=1 
+   # and grid (m,n) assigned to class c = m*n.
   for i in range(len(loc)):
       c[i] = (xnew[i]-1)+xnum*(ynew[i]-1)
   c = np.reshape(c,(len(c),1))
 
+   # M - ADP
+   # c - class
+  # location (x,y)
   return loc,c, M
 
 
-
+# For each grid, collect all ADP samples from the training set that belong to the same grid into one group. 
+# This means that grid will have a collection of ADPs that belong to this grid and this collection will be used for KNN. 
 def get_sub(subclass,M,c,x,y):
   xnum = 18
   ynum = 55
@@ -79,9 +92,10 @@ def get_sub(subclass,M,c,x,y):
   Mnew = np.zeros([n,64,64])
   xlocnew = np.zeros(n)
   ylocnew = np.zeros(n)
-  # find all values of subclass
+  
   j=0
-
+  
+# for each class i compute the ADP, and (x,Y) location
   for i in range((len(c))):
     if (c[i]==subclass):
       Mnew[j,:,:]=M[i,:,:]
@@ -98,7 +112,8 @@ def get_sub(subclass,M,c,x,y):
   ymax = max(ylocnew)
   ymin = min(ylocnew)
   ystep = (ymax-ymin)/ynum
-
+  
+  #computes new x and y locationa based on the grif
   for i in range(n):
       for j in range(1,xnum+1):
           low = xmin + (j-1)*xstep
@@ -116,24 +131,29 @@ def get_sub(subclass,M,c,x,y):
               ynew[i] = j
           if(y[i] == ymax):
               ynew[i] = ynum
-
+  #computes class based on grid
   for i in range(len(c1)):
       c1[i] = (xnew[i]-1)+xnum*(ynew[i]-1)
 
   c1 = np.reshape(c1,(n,1))
-
+  # Mnew -ADP
+  # c1- class
+  #xlocnew and ylocnew are x and y coordinates
   return Mnew, c1,xlocnew,ylocnew
 
-
+#loads Train Dataset
 data_path1 ='Data/TrainDCNNI1.npz'
 data1 = np.load(data_path1)
 
+#Takes the dataset and grid size as input
+#outputs ADP, class, and location(x,y)
 loc_t, c_t, ADP_t= get_valid_data(data1,xnum,ynum,1)
 num_classes = xnum*ynum
 x_loc = loc_t[:,0]
 y_loc = loc_t[:,1]
 
 for sub in range(num_classes):
+  #For every class on the grid it creates a dataset of ADP, class, and locatioon
    train_ADP, train_c, xtrain, ytrain = get_sub(sub,ADP_t,c_t,x_loc,y_loc)   
    np.save('ADP/ADP'+str(sub),train_ADP)
    np.save('ADP/class'+str(sub),train_c)
