7834820643d6e11aaa58a79e841b5d02e9a442a9
FarzamHejaziK <77290908+FarzamHejaziK@users.noreply.github.com>
2021-03-17 17:31:33 -0400

    Update Dataset_gen.m

--- Matlab Codes/Indoor/Dataset_gen.m
+++ Matlab Codes/Indoor/Dataset_gen.m
@@ -5,25 +5,6 @@ W = 100; % length of the test Dataset, e.g. from R100 to R200
 L = 121; % length of each rows (from DeepMIMO website)
 Nt = 32 ;  % Number of antennas at BS
 Nc = 32 ;  % Number of Subcarriers
-
-for iii = 1 : 100 * 121
-clearvars -except iii DeepMIMO_dataset iji 
-iii
-N = 20;  % Number of Frames
-W = 100; % length of the test Dataset, e.g. from R100 to R200
-L = 121; % length of each rows (from DeepMIMO website)
-Nt = 32 ;  % Number of antennas at BS
-Nc = 32 ;  % Number of Subcarriers 
-Thr1 = 50;
-Thr2 = 10;
-
-
-% reading dataset from struct array
-H = DeepMIMO_dataset{1}.user{iii}.channel;
-Loc = DeepMIMO_dataset{1}.user{iii}.loc;
-
- 
-
 %% Constructing V
 V = zeros(Nt,Nt);
 
@@ -43,10 +24,31 @@ for i = 1 : Nc
 end 
 
 
+for iii = 1 : 100 * 121
+clearvars -except iii DeepMIMO_dataset iji 
+iii
+N = 20;  % Number of Frames
+W = 100; % length of the test Dataset, e.g. from R100 to R200
+L = 121; % length of each rows (from DeepMIMO website)
+Nt = 32 ;  % Number of antennas at BS
+Nc = 32 ;  % Number of Subcarriers 
+Thr1 = 50;
+Thr2 = 10;
+
+
+% reading dataset from struct array
+H = DeepMIMO_dataset{1}.user{iii}.channel;
+Loc = DeepMIMO_dataset{1}.user{iii}.loc;
+
+ 
+
+
+
+
 %% Constructing Angle-delay profiles 
 
 ADP = V' * H * F ;
-imagesc(abs(ADP))
+
 dlmwrite('TrainDataADP.csv',abs(ADP),'delimiter',',','-append','precision',4);
 dlmwrite('TrainDataLoc.csv',Loc,'delimiter',',','-append','precision',4);
 
